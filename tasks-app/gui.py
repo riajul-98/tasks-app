5056ffb01c18f750457753776d7c1d1c4437f62a
Riajul <riajul5@hotmail.co.uk>
2025-12-16 17:53:12 +0000

    Implemented edit button

--- tasks-app/gui.py
+++ tasks-app/gui.py
@@ -4,8 +4,10 @@ import FreeSimpleGUI as sg
 label = sg.Text("Type in a task")
 input_box = sg.InputText(tooltip="Enter task", key="task")
 add_botton = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_tasks(), key="tasks", enable_events=True, size=[45, 10])
+edit_button = sg.Button("Edit")
 
-window = sg.Window("My Tasks App", layout=[[label, input_box, add_botton]], font=("Helvetica", 20))
+window = sg.Window("My Tasks App", layout=[[label, input_box, add_botton], [list_box, edit_button]], font=("Helvetica", 15))
 
 while True:
     event, values = window.read()
@@ -18,6 +20,20 @@ while True:
             new_task = values["task"] + "\n"
             tasks.append(new_task)
             functions.write_tasks(tasks)
+            window["tasks"].update(values=tasks)
+        case "Edit":
+            task_to_edit = values["tasks"][0]
+            new_task = values["task"]
+
+            tasks = functions.get_tasks()
+            index = tasks.index(task_to_edit)
+            tasks[index] = new_task
+
+            functions.write_tasks(tasks)
+            window["tasks"].update(values=tasks)
+        case "tasks":
+            window["task"].update(value=values["tasks"][0])
+
         case sg.WIN_CLOSED:
             break
 
